310bb9e865e396e69a667fb9ec9d936cf47c895d
Pongsak Suvanpong <psksvp@gmail.com>
2018-06-14 13:35:32 +1000

    add more test cases to RepetitionPairs

--- OverlappingPair.scala
+++ OverlappingPair.scala
@@ -2,15 +2,16 @@
 
 case class RepetitionPairs(pl:List[(Int, Int)])
 {
-  lazy val overlap = (for(p <- xvariations(pl, 2) if cover(p.head, p.last)) yield p.last).toList
+  lazy val coveredPairs = (for(p <- xvariations(pl, 2) if cover(p.head, p.last)) yield p.last).toList
+  lazy val nonOverlapPairs = (pl diff coveredPairs).sorted
 
   // check if p1 covers p2
   def cover(p1:(Int, Int), p2:(Int, Int)): Boolean=
   {
     val (p1Start, p1End) = p1
     val (p2Start, p2End) = p2
-    p2Start > p1Start && p2Start < p1End &&
-    p2End > p1End
+    p2Start > p1Start && p2Start < p1End && //start of p2 is inside p1
+    p2End > p1End                           // end of p2 is outside p1
   }
 
   //code taken from
@@ -44,7 +45,17 @@ object Entry
 {
   def main(args:Array[String]):Unit=
   {
-    println(RepetitionPairs(List((1, 3), (5, 7), (9, 12), (10, 13))).overlap)
-    println(RepetitionPairs(List((1, 3), (5, 8), (6, 9))).overlap)
+    
+    println(RepetitionPairs(List((1, 3), (5, 7), (9, 12), (10, 13))).coveredPairs)
+    println(RepetitionPairs(List((1, 3), (5, 7), (9, 12), (10, 13))).nonOverlapPairs)
+    println(RepetitionPairs(List((1, 3), (5, 8), (6, 9))).coveredPairs)
+    println(RepetitionPairs(List((1, 3), (5, 8), (6, 9))).nonOverlapPairs)
+    println(RepetitionPairs(List((3,5), (7,10), (8,11))).coveredPairs)
+    println(RepetitionPairs(List((3,5), (7,10), (8,11))).nonOverlapPairs)
+    println(RepetitionPairs(List((7,10), (1,3), (8,11))).coveredPairs)
+    println(RepetitionPairs(List((7,10), (1,3), (8,11))).nonOverlapPairs)
+     
+    println(RepetitionPairs(List((5,7), (9,12), (1,3), (10,13))).coveredPairs)
+    println(RepetitionPairs(List((5,7), (9,12), (1,3), (10,13))).nonOverlapPairs)
   }
 }
